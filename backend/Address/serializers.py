30c4567517dad4db90d260e0078f802188f27b07
Wallison <wallisonzwka@gmail.com>
2024-06-16 15:52:16 -0300

    feat/address

---
+++ backend/Address/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import AddressModel
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = AddressModel
+        fields = '__all__'
